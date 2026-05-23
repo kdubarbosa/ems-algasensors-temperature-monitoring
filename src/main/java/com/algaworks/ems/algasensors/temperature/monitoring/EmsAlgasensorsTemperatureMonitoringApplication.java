09697e1c444b8fc68886a201a0a1d417fae5711b
Carlos Eduardo Barbosa <kdubpf@gmail.com>
2026-05-23 14:03:06 -0300

    novos arquivos

---
+++ src/main/java/com/algaworks/ems/algasensors/temperature/monitoring/EmsAlgasensorsTemperatureMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.ems.algasensors.temperature.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsAlgasensorsTemperatureMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsAlgasensorsTemperatureMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/ems/algasensors/temperature/monitoring/EmsAlgasensorsTemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.ems.algasensors.temperature.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsAlgasensorsTemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
